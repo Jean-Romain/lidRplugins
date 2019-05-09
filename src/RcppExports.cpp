c42cf582ca28b315bf4166f88d727cd80866fafa
Jean-Romain <jean-romain.roussel.1@ulaval.ca>
2019-05-09 13:53:14 -0400

    Remove lascoplanar

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,20 +6,6 @@
 
 using namespace Rcpp;
 
-// C_lascoplanar
-LogicalVector C_lascoplanar(S4 las, int k, double th1, double th2);
-RcppExport SEXP _lidRplugins_C_lascoplanar(SEXP lasSEXP, SEXP kSEXP, SEXP th1SEXP, SEXP th2SEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< S4 >::type las(lasSEXP);
-    Rcpp::traits::input_parameter< int >::type k(kSEXP);
-    Rcpp::traits::input_parameter< double >::type th1(th1SEXP);
-    Rcpp::traits::input_parameter< double >::type th2(th2SEXP);
-    rcpp_result_gen = Rcpp::wrap(C_lascoplanar(las, k, th1, th2));
-    return rcpp_result_gen;
-END_RCPP
-}
 // C_hamraz_segmentation
 List C_hamraz_segmentation(S4 disc, double nps, int sensitivity, double MDCW, double Epsilon, double CLc, double CLs, double Oc, double Os, double radius);
 RcppExport SEXP _lidRplugins_C_hamraz_segmentation(SEXP discSEXP, SEXP npsSEXP, SEXP sensitivitySEXP, SEXP MDCWSEXP, SEXP EpsilonSEXP, SEXP CLcSEXP, SEXP CLsSEXP, SEXP OcSEXP, SEXP OsSEXP, SEXP radiusSEXP) {
@@ -57,7 +43,6 @@ END_RCPP
 }
 
 static const R_CallMethodDef CallEntries[] = {
-    {"_lidRplugins_C_lascoplanar", (DL_FUNC) &_lidRplugins_C_lascoplanar, 4},
     {"_lidRplugins_C_hamraz_segmentation", (DL_FUNC) &_lidRplugins_C_hamraz_segmentation, 10},
     {"_lidRplugins_C_lastrees_ptrees", (DL_FUNC) &_lidRplugins_C_lastrees_ptrees, 5},
     {NULL, NULL, 0}
